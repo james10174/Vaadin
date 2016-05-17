c705165d1a78d3581398d4f0167ace28da4fef04
james10174 <james10174@gmail.com>
2016-05-17 21:39:55 +0000

    5

---
+++ src/main/java/com/honeycomb/MyComponent.java
@@ -0,0 +1,7 @@
+package com.honeycomb;
+
+import com.vaadin.ui.AbstractComponent;
+
+public class MyComponent extends AbstractComponent {
+
+}
---
+++ src/main/java/com/honeycomb/MyComponentState.java
@@ -0,0 +1,8 @@
+package com.honeycomb;
+
+
+import com.vaadin.shared.AbstractComponentState;
+
+public class MyComponentState extends AbstractComponentState {
+    public String text;
+}
