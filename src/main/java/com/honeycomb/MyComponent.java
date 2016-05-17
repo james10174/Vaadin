af5573abb53e9d10bbe7d78c037a4a7bec0fb230
james10174 <james10174@gmail.com>
2016-05-17 22:13:29 +0000

    11

--- src/main/java/com/honeycomb/MyComponent.java
+++ src/main/java/com/honeycomb/MyComponent.java
@@ -1,7 +1,22 @@
 package com.honeycomb;
 
 import com.vaadin.ui.AbstractComponent;
+import com.honeycomb.client.MyComponentServerRpc;
+import com.vaadin.ui.Notification;
+import com.vaadin.shared.MouseEventDetails;
 
 public class MyComponent extends AbstractComponent {
+    
+        public MyComponent() {
+            MyComponentServerRpc rpc =
+            new MyComponentServerRpc() {
+                private int clickCount = 0;
+        
+                public void clicked(MouseEventDetails mouseDetails) {
+                    Notification.show("Clicked " + mouseDetails.getButtonName());
+                }
+            };
+            registerRpc(rpc);
+        }
 
 }
--- target/classes/com/honeycomb/client/MyComponentServerRpc.java
+++ target/classes/com/honeycomb/client/MyComponentServerRpc.java
@@ -1,4 +1,4 @@
-package com.honeycomb;
+package com.honeycomb.client;
 
 import com.vaadin.shared.MouseEventDetails;
 import com.vaadin.shared.communication.ServerRpc;
@@ -6,5 +6,6 @@ import com.vaadin.shared.communication.ServerRpc;
 public interface MyComponentServerRpc extends ServerRpc {
 
     public void clicked(MouseEventDetails mouseDetails);
+    //mouseDetails.getButtonName()
 
 }
