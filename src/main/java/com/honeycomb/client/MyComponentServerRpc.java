e2fe9430e557d4194761b91d51ce0f2539d6edda
james10174 <james10174@github.com>
2016-05-29 06:30:00 +0000

    Working with debugger

--- src/main/java/com/honeycomb/client/MyComponentServerRpc.java
+++ src/main/java/com/honeycomb/client/MyComponentServerRpc.java
@@ -4,7 +4,7 @@ import com.vaadin.shared.MouseEventDetails;
 import com.vaadin.shared.communication.ServerRpc;
 
 public interface MyComponentServerRpc extends ServerRpc {
-
+    
     public void clicked(MouseEventDetails mouseDetails);
     //mouseDetails.getButtonName()
 
