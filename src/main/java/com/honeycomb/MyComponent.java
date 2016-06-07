a5a0ff363631138702d5e3eb1d4e19b6d6547d46
james10174 <redder.2009@gmail.com>
2016-06-07 17:53:52 +0000

    Separate click function for debugging.

--- src/main/java/com/honeycomb/MyComponent.java
+++ src/main/java/com/honeycomb/MyComponent.java
@@ -6,17 +6,19 @@ import com.vaadin.ui.Notification;
 import com.vaadin.shared.MouseEventDetails;
 
 public class MyComponent extends AbstractComponent {
-    
+        private int clickCount = 0;
         public MyComponent() {
             MyComponentServerRpc rpc =
             new MyComponentServerRpc() {
-                private int clickCount = 0;
-        
                 public void clicked(MouseEventDetails mouseDetails) {
-                    Notification.show("Clicked " + clickCount++  +" " + mouseDetails.getButtonName());
+                    (new MyComponent()).notify(mouseDetails);
                 }
             };
+            
             registerRpc(rpc);
         }
+        public void notify(MouseEventDetails mouseDetails){
+            Notification.show("Clicked " + clickCount++  +" " + mouseDetails.getButtonName());
+        };
 
 }
